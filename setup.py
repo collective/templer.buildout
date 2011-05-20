e9e74bd31f260ce37d3763ca97bd4e0c508312b8
cewing <cewing@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-05-20 18:35:22 +0000

    remove test_suite since it seems to force our tests to run twice
    git-svn-id: https://svn.plone.org/svn/collective/templer.buildout/trunk@239717 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- setup.py
+++ setup.py
@@ -43,7 +43,6 @@ setup(name='templer.buildout',
           'templer.core',
       ],
       tests_require=tests_require,
-      test_suite='templer.buildout.tests.test_all.test_suite',
       extras_require=dict(test=tests_require),
       entry_points="""
       [paste.paster_create_template]
